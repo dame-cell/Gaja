a96fa7496aebb71bf21f178dc0d8cf153018d537
win10 <doss72180@gmail.com>
2024-03-06 20:05:17 +0530

    fixing some errors in the training sciprt

--- src/finetune/training.py
+++ src/finetune/training.py
@@ -31,6 +31,22 @@ def format_chat_ml(tokenizer, dataset):
     dataset = dataset.map(formatting_prompts_func, batched=True)
     return dataset
 
+def get_peft_model(model, r, lora_alpha):
+    peft_model = FastLanguageModel.get_peft_model(
+        model,
+        r=r, 
+        target_modules=["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+        lora_alpha=lora_alpha,
+        lora_dropout=0, 
+        bias="none",    
+        use_gradient_checkpointing=True,
+        random_state=3407,
+        use_rslora=False,  
+        loftq_config=None, 
+    )
+    return peft_model
+
 def training(model, tokenizer, dataset, max_seq_length, dataset_num_proc, save_total_limit, output_dir, args):
     trainer = SFTTrainer(
         model=model,
@@ -45,9 +61,10 @@ def training(model, tokenizer, dataset, max_seq_length, dataset_num_proc, save_t
             gradient_accumulation_steps=args.gradient_accumulation_steps,
             warmup_steps=args.warmup_steps,
             max_steps=args.max_steps,
+            hub_token = args.token,
             push_to_hub=True,
             hub_strategy="checkpoint",
-            push_to_hub_model=args.push_to_hub_model,
+            push_to_hub_model_id=args.push_to_hub_model,
             learning_rate=args.learning_rate,
             fp16=not torch.cuda.is_bf16_supported(),
             bf16=torch.cuda.is_bf16_supported(),
@@ -65,7 +82,7 @@ def training(model, tokenizer, dataset, max_seq_length, dataset_num_proc, save_t
 
 def main(args):
     model, tokenizer = loading_model(args.modelname, args.max_seq_length, args.dtype, args.load_in_4bit)
-
+    model = get_peft_model(model,args.r, args.lora_alpha)
     if args.dataset:
         dataset = load_dataset(args.dataset, split="train")
         dataset = dataset.select(range(args.range))
@@ -90,7 +107,7 @@ if __name__ == "__main__":
     parser.add_argument("--gradient_accumulation_steps", type=int, default=4)
     parser.add_argument("--warmup_steps", type=int, default=5)
     parser.add_argument("--max_steps", type=int, default=500)
-    parser.add_argument("--push_to_hub_model", type=str, default="your-model-name")
+    parser.add_argument("--push_to_hub_model", type=str, default="last-hopefully")
     parser.add_argument("--learning_rate", type=float, default=2e-4)
     parser.add_argument("--logging_steps", type=int, default=1)
     parser.add_argument("--save_steps", type=int, default=100)
@@ -98,6 +115,9 @@ if __name__ == "__main__":
     parser.add_argument("--weight_decay", type=float, default=0.01)
     parser.add_argument("--lr_scheduler_type", type=str, default="linear")
     parser.add_argument("--seed", type=int, default=3407)
+    parser.add_argument("--token", type=str, default=None)
+    parser.add_argument("--r", type=int, default=16)
+    parser.add_argument("--lora_alpha", type=int, default=16)
 
     args = parser.parse_args()
     main(args)
